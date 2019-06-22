50dc478452fba675c5abb15c20ddf06f9b17b780
cmeo5xmm <cmeo5xmm@gmail.com>
2019-06-22 15:16:26 +0800

    Update Two Sum.cpp

--- 1. Two Sum/Two Sum.cpp
+++ 1. Two Sum/Two Sum.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> hash;
         for(int i=0;i<nums.size();i++){
-            int re=target-nums[i];
-            for(int j=i+1;j!=nums.size();j++){
-                if(re==nums[j]){
-                    vector<int> r(2);
-                    r[0]=i;
-                    r[1]=j;
-                    return r;
-                }
+            if(hash.count(target-nums[i])){
+                return {hash[target-nums[i]],i};
             }
+            hash[nums[i]]=i;
         }
+        return {};
     }
-
 };
